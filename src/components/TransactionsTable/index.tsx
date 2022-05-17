430ada084ea0c2c361267ab38f57ddd52de6c442
Cardoso <cardosoassuncao@gmail.com>
2022-05-17 13:20:46 -0400

    add component TransactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Criação landing page</td>
+            <td className="deposit">R$800,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Conta de energia elétrica</td>
+            <td className="withdraw" >- R$500,00</td>
+            <td>Casa</td>
+            <td>22/02/2021</td>
+          </tr>
+          <tr>
+            <td>Vendas</td>
+            <td className="deposit">R$200,00</td>
+            <td>Extra</td>
+            <td>27/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
