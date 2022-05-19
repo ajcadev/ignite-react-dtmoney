55a90a6be04f4aafad37d89592966f5f67b37e94
Cardoso <cardosoassuncao@gmail.com>
2022-05-19 19:24:58 -0400

    finalize Context API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,8 +4,9 @@ import { Container, RadioBox, TransactionTypeContainer } from './styles'
 import incomeImg from "../../assets/income.svg"
 import outcomeImg from "../../assets/outcome.svg"
 import closeImg from "../../assets/close.svg"
-import { FormEvent, useState } from 'react'
+import { FormEvent, useContext, useState } from 'react'
 import { api } from '../../services/api'
+import { TransactionsContext } from '../../TransactionsContext'
 
 type NewTransactionModalProps = {
   isOpen: boolean
@@ -13,20 +14,25 @@ type NewTransactionModalProps = {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const { createTransaction } = useContext(TransactionsContext)
   const [title, setTitle] = useState("")
   const [amount, setAmount] = useState(0)
   const [category, setCategory] = useState("")
   const [type, setType] = useState('deposit')
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
-    const data = {
+    await createTransaction({
       title,
       amount,
       category,
       type
-    }
-    api.post('/transactions', data)
+    })
+    setTitle('')
+    setAmount(0)
+    setCategory('')
+    setType('deposit')
+    onRequestClose()
   }
 
   return (
