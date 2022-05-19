9228ffef0dbf563b80b00be871cc191bdb70a839
Cardoso <cardosoassuncao@gmail.com>
2022-05-19 15:24:24 -0400

    add Context API

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,7 +2,11 @@ import { Container } from "./styles";
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
+import { useContext } from "react";
+import { TransactionsContext } from "../../TransactionsContext";
 export function Summary() {
+  const transactions = useContext(TransactionsContext)
+
   return (
     <Container>
       <div>
@@ -10,21 +14,21 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>{new Intl.NumberFormat("pt-BR", {style: "currency",currency: "BRL"}).format(1000)}</strong>
       </div>
       <div>
         <header>
           <p>Saidas</p>
           <img src={outcomeImg} alt="Saidas" />
         </header>
-        <strong>- R$500,00</strong>
+        <strong>{new Intl.NumberFormat("pt-BR", {style: "currency",currency: "BRL"}).format(-500)}</strong>
       </div>
       <div>
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>{new Intl.NumberFormat("pt-BR", {style: "currency",currency: "BRL"}).format(500)}</strong>
       </div>
     </Container>
   )
